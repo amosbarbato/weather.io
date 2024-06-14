3b2c4d1a279b302c59e8a177864141747adea196
Amos Barbato <amosbarbato@gmail.com>
2024-06-14 12:26:29 -0300

    feat: removo e fix scss files

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,16 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-undef */
-
 import { useEffect, useState } from 'react'
-import { Highlight } from './components/Highlight'
 import getFormattedWeatherData from './services/weatherService'
 
-import './styles.scss'
-import { MagnifyingGlass } from "@phosphor-icons/react"
-
-import { Forecast } from './components/Forecast'
-import { Header } from './components/Header'
+import { Actions } from './components/Actions'
 import { CurrentWeather } from './components/CurrentWeather'
-
+import { Highlight } from './components/Highlight'
+import { Forecast } from './components/Forecast'
 
 
 const App = () => {
-
 	const [query, setQuery] = useState({ q: "rio de janeiro" })
-	const [units, setUnits] = useState("metric")
+	const [units] = useState("metric")
 	const [weather, setWeather] = useState(null)
-
 
 	const getWeather = async () => {
 		await getFormattedWeatherData({ ...query, units }).then((data) => {
@@ -33,85 +23,24 @@ const App = () => {
 	}, [query, units])
 
 
-	
 	return (
 		<div className="App">
 				<main>
 					<article className='container'>
 
 						{weather && (
-							<>
-							
+							<>						
 								<div className="content-left card">
-									<Header setQuery={setQuery} />		
-											
+									<Actions setQuery={setQuery} />
 									<CurrentWeather weather={weather} />
 								</div>
 
 								<div className="content-right container">
 									<Highlight weather={weather} />
-
 									<Forecast data={weather.daily} /> 
 								</div>
-
-
-								{/* <section className="load-search">
-
-									<div className="header">
-										<a href="#" className='logo'>
-											<img src="../logotype.svg" alt=""/>
-										</a>
-									</div>
-
-									<div className="load-search-content">
-										<div className="welcome">
-											<h2 className="heading-md">Boas vindas ao <span>TypeWeather</span></h2>
-											<p className='text-md'>Escolha um local para ver a previsão do tempo</p>
-										</div>
-
-										<div className="search-view">
-											<input 
-												// value={city}
-												// onChange={(e) => setCity(e.currentTarget.value)}
-												type="search" 
-												name='search' 
-												placeholder='Buscar local' 
-												className='search-field'
-											/>
-											<button>
-												<MagnifyingGlass size={24} color='white'/>
-											</button>
-										</div>
-
-										<div className="search-result ">
-											<ul className="view-list">
-
-												<li className="view-item">
-													<p className="text-md">Porto Alegre, RS - Brasil</p>
-													<a href="#" className="item-link"></a>
-												</li>
-
-												<li className="view-item">
-													<p className="text-md">Porto Seguro, BA - Brasil</p>
-													<a href="#" className="item-link"></a>
-												</li>
-
-												<li className="view-item">
-													<p className="text-md">Porto - Portugal</p>
-													<a href="#" className="item-link"></a>
-												</li>
-											</ul>
-										</div>
-									</div>
-
-									
-								</section> */}
-							</>
-							
+							</>							
 						)}
-						
-						
-						<div className="loading"></div>
 
 					</article>
 				</main>
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './styles.scss'
+import './styles/main.scss'
 import { BrowserRouter as Router } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
